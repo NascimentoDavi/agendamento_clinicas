bfe6a6f3bbe34554800da00ac1ef91320794a474
vinicgobbi <vinicius.cgobbi2004@gmail.com>
2025-09-17 12:48:16 -0300

    refactor: remove data padrão dos seletores de data
    Elimina a definição automática da data atual nos campos de seleção,
    permitindo maior flexibilidade na escolha das datas pelo usuário.

--- resources/views/psicologia/professor/consultar_agenda.blade.php
+++ resources/views/psicologia/professor/consultar_agenda.blade.php
@@ -343,7 +343,6 @@
             altInput: true,
             altFormat: "d/m/Y",
             allowInput: true,
-            defaultDate: "today"
         });
 
         const startTimePicker = flatpickr("#start-time-input", {
